313b8b79f17eb1c9c62cc38da18e5d98b0189932
katkar213 <blackship213@gmail.com>
2023-10-27 23:37:07 +0530

    just commited

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,26 +1,9 @@
 import React ,{useEffect}from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+
 
 function Home()
 {
-    const navigate = useNavigate();
-    const token = localStorage.getItem("token");
-    console.log(token);
-    useEffect(()=>{
-        if(token){
-        axios.get("http://localhost:4001/api/home",{headers:{"authorization":`Bearer ${token}`}})
-        .then(res=>console.log(res.data))
-        }
-        else{
-            navigate("/login")
-        }
-    },[token,navigate]);
-
-    const handleClick= ()=>{
-        localStorage.removeItem("token");
-        navigate('/login')
-    }
+    
     return(
         <>
             <h1>Home Page</h1>
